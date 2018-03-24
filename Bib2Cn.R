12322300438d1bbe438542cfbc78a64bec2d402f
Jingmeng Cui <942862946@qq.com>
2018-03-24 11:29:05 +0800

    first publishment

---
+++ Bib2Cn.R
@@ -0,0 +1,43 @@
+Bib2Cn <- function(file_in, file_out){
+  if(! "RefManageR" %in% installed.packages()){
+    install.packages("RefManageR")
+  }
+  if(! "tidyverse" %in% installed.packages()){
+    install.packages("tidyverse")
+  }
+  library(RefManageR)
+  library(tidyverse)
+  temp <- readLines(file_in, encoding = "UTF-8")
+  temp[str_detect(temp, "author.*[\u4E00-\u9FA5]")] <- 
+    str_replace_all(
+      temp[str_detect(temp, "author.*[\u4E00-\u9FA5]")],
+      c(",\\ " = "\\{, \\}",
+        "\\ \\\\\\%A\\}" = "\\}",
+        "\\ \\\\\\%A\\ " = "\\{, \\}",
+        "\\ and\\ " = "\\{, \\}"
+        )
+    )
+  
+  for(i in 1:length(temp)){
+    if(str_detect(temp[i], "@")){
+      j <- i
+      
+      while(1 == 1){
+        if(str_detect(temp[j], "author")){
+          if(str_detect(temp[j], "[\u4E00-\u9FA5]+")){
+            temp[i] <- str_replace(temp[i], "_", str_extract(temp[j], "[\u4E00-\u9FA5]+"))
+          }
+          break()
+        }
+        j <- j+1
+      }
+      
+    }
+  }
+  
+  write.table(temp, 
+              file = file_out, row.names = F, quote = F, sep="\n",
+              fileEncoding = "UTF-8")
+}
+
+
